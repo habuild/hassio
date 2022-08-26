3c74d9653d50feaf49bbe1b2ddf836ff14ebdce7
habuild <36288425+habuild@users.noreply.github.com>
2022-08-26 13:12:14 +1000

    Update mqttSensorConfig.sh

--- ether-sbfspot/rootfs/usr/bin/sbfspot/mqttSensorConfig.sh
+++ ether-sbfspot/rootfs/usr/bin/sbfspot/mqttSensorConfig.sh
@@ -6,11 +6,11 @@ CONFIG_PATH=/data/options.json
 
 #  Wait for for first publish to get serial
 
-CONFIG_MQTT_HOST="$(jq --raw-output '.MQTT_Host' $CONFIG_PATH)"
-CONFIG_MQTT_USER="$(jq --raw-output '.MQTT_User' $CONFIG_PATH)"
-CONFIG_MQTT_PASS="$(jq --raw-output '.MQTT_Pass' $CONFIG_PATH)"
-CONFIG_MQTT_TOPIC="$(jq --raw-output '.MQTT_Topic' $CONFIG_PATH)"
-CONFIG_MQTT_DATA="$(jq --raw-output '.MQTT_Data' $CONFIG_PATH)"
+MQTT_Host="$(jq --raw-output '.MQTT_Host' $CONFIG_PATH)"
+MQTT_User="$(jq --raw-output '.MQTT_User' $CONFIG_PATH)"
+MQTT_Pass="$(jq --raw-output '.MQTT_Pass' $CONFIG_PATH)"
+MQTT_Topic="$(jq --raw-output '.MQTT_Topic' $CONFIG_PATH)"
+MQTT_Data="$(jq --raw-output '.MQTT_Data' $CONFIG_PATH)"
 
 echo "$MQTT_HOST"
 echo "$MQTT_USER
@@ -23,4 +23,4 @@ echo "$MQTT_DATA"
 
 #  InvName delete
 #  -h {host} -u {MQTT_User} -P {MQTT_Pass} -t {topic} -m
-/usr/bin/mosquitto_pub -h "$MQTT_HOST" -u "$MQTT_USER" -P "$MQTT_PASS" -t homeassistant/sensor/sbfspot_HasSMA5000TL-20/sbfspot_2100443252InvName/config -m "" -d
+/usr/bin/mosquitto_pub -h "$MQTT_Host" -u "$MQTT_User" -P "$MQTT_Pass" -t homeassistant/sensor/sbfspot_HasSMA5000TL-20/sbfspot_2100443252InvName/config -m "" -d
