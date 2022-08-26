1d97582dadb3e1bfb84150bcb95507f34beaec9a
habuild <36288425+habuild@users.noreply.github.com>
2022-08-26 17:10:31 +1000

    Update mqttSensorConfig.sh

--- ether-sbfspot/rootfs/usr/bin/sbfspot/mqttSensorConfig.sh
+++ ether-sbfspot/rootfs/usr/bin/sbfspot/mqttSensorConfig.sh
@@ -25,6 +25,24 @@ echo "$MQTT_Data"
 
 #  topic= insert into existing topic /sensor/ varJson
 
+#   ALL Values
+#   MQTT_Data=Timestamp,InvTime,SunRise,SunSet,InvSerial,InvName,InvClass,InvType,InvSwVer,InvStatus,InvTemperature,InvGridRelay,EToday,ETotal,PACTot,UDC1,UDC2,IDC1,IDC2,PDC1,PDC2,PDCTot,OperTm,FeedTm,PAC1,PAC2,PAC3,UAC1,UAC2,UAC3,IAC1,IAC2,IAC3,GridFreq,BTSignal,BatTmpVal,BatVol,BatAmp,BatChaStt
+
+
 #  InvName delete
 #  -h {host} -u {MQTT_User} -P {MQTT_Pass} -t {topic} -m
 /usr/bin/mosquitto_pub -h "$MQTT_Host" -u "$MQTT_User" -P "$MQTT_Pass" -t homeassistant/sensor/sbfspot_"$PLANTNAME"InvName/config -m "" -d
+/usr/bin/mosquitto_pub -h core-mosquitto -u hasmqtt -P pas0th5 -t homeassistant/sensor/sbfspot_"$PLANTNAME"/sbfspot_2100443252"$value"/config -m '{ "name": "SMA Inverter Nom", "state_topic": "homeassistant/sbfspot_HasSMA5000TL-20/sbfspot_2100443252", "value_template": "{{ value_json.InvName }}", "unique_id": "2100443252_{$value}", "device": { "identifiers": ["sbfspot_HasSMA5000TL-20/sbfspot_2100443252/"], "name": "SMA ", "model": "SMA5000", "manufacturer": "SMA", "sw_version": "1.X" } }' -d
+
+if bashio::var.has_value "${MQTT_Data}" "Timestamp" ; then
+   echo 'has Timestamp'
+fi
+
+if bashio::var.has_value "${MQTT_Data}" "SunRise" ; then
+     echo 'do SunRise sensor'
+fi
+
+if bashio::var.has_value "${MQTT_Data}" "SunSet" ; then
+     echo 'do SunSet'
+
+fi
