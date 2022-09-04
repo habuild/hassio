dc3d05ac4dde7197857b8fdcdb15d3915d63f5d3
habuild <36288425+habuild@users.noreply.github.com>
2022-09-04 19:04:28 +1000

    Update genBluetoothConfig.sh

--- ether-sbfspot/rootfs/usr/bin/sbfspot/genBluetoothConfig.sh
+++ ether-sbfspot/rootfs/usr/bin/sbfspot/genBluetoothConfig.sh
@@ -2,7 +2,7 @@
 
 CFG_PATH="${1}"
 CFG_PATH_UPLOAD="${2}"
-CONFIG_PATH=/data/options.json
+#CONFIG_PATH=/data/options.json
 
 # CONFIG_CONNECTION_TYPE="$(bashio::config 'Connection_Type')"
 CONFIG_BTADDRESS="$(bashio::config 'BTAddress' '!secret BTAdress')"
